4e5b0a0258b60a6b942acb44776fce27bc14f59b
Jack Ponder <jackponder3@gmail.com>
2023-07-25 13:12:16 -0400

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<div class="text-center">
+  <h1 class="text-[5vmin] mt-8">
+    Whoops! Something went wrong!
+  </h1>
+  <h3 class="text-[20vmin] mb-4">
+    ¯\_(ツ)_/¯
+  </h3>
+  <a href="/" class="text-[2.5vmin] hover:underline">
+    Navigate back to homepage
+  </a>
+</div>
