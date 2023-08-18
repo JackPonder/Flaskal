6978be3adc2cc9d138c8b2fa6c0ed2ddd6124082
Jack <jackponder3@gmail.com>
2023-08-18 16:19:51 -0400

    fixed comment links in user page

--- src/routes/users/[slug]/comments/+page.svelte
+++ src/routes/users/[slug]/comments/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 {#each data.comments as comment}
-  <Comment {comment} deletable={data.currentUser?.username === comment.creator} />
+  <Comment {comment} link="Poll" deletable={data.currentUser?.username === comment.creator} />
 {:else}
   <div class="card p-4 text-center">
     Looks like {data.user.username} hasn't made any comments
