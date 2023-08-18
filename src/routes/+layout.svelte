ee01f5483c6b2f9dd179c0ab3a279e91700d18c3
Jack <jackponder3@gmail.com>
2023-08-17 20:13:21 -0400

    hid scrollbar when modals are open

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,28 @@
-<script>
-  import { Navbar, Alert, LoginForm, RegisterForm } from "$lib/components";
+<script lang="ts">
+  import type { Action } from "svelte/action";
   import "../app.css";
+
+  import { Navbar, Alert, LoginForm, RegisterForm } from "$lib/components";
+  import { modals } from "$lib/stores";
+
+  const bodyClass: Action<HTMLBodyElement, {
+    className: string;
+    toggle: boolean;
+  }> = (node, { className, toggle }) => {
+    node.classList.toggle(className, toggle);
+
+    return {
+      update: ({ className, toggle }) => {
+        node.classList.toggle(className, toggle);
+      }
+    };
+  };
 </script>
+
+<svelte:body use:bodyClass={{ 
+  className: "overflow-hidden", 
+  toggle: $modals !== 0,
+}} />
 
 <Navbar />
 <Alert />
